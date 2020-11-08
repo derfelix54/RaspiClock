9432cee4e56b89f8afed2b2dd8fb8476b3dae086
felix <reichlin@hm.edu>
2020-11-08 14:14:09 +0100

    initial project setup

---
+++ gui.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+MainWindow
+Window where Clock and Date should appear
+
+
+@author: Felix Reichling
+"""
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        """
+        docstring
+        """
+        pass
---
+++ main.py
@@ -0,0 +1,24 @@
+"""
+@author: Felix Reichling
+"""
+
+
+
+from PyQt5.QtWidgets import QApplication
+from gui import MainWindow
+import sys
+
+
+
+"""
+Main function to start the gui
+"""
+
+if __name__ == "__main__":
+
+
+    app = QApplication(sys.argv)
+    screen_res = app.desktop().screenGeometry()
+    height = screen_res.height()
+
+    m = MainWindow()
